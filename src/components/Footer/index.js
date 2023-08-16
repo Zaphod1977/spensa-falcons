833a08c6c4314fcc9cc04e6a7024bf531cddd568
carfiguru@hotmail.com <carfiguru@hotmail.com>
2023-08-15 23:42:47 -0500

    added github link to bsquared cred

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
+
 function Footer() {
   return (
     <footer className='space'>
         <div id="con">
-            <a>by...B-Squared</a>
-            {/* <a href="https://www.linkedin.com/in/bobby-bridges-baa54889/" target="_blank">Linkedin</a>
-            <a href="https://github.com/Zaphod1977/" target="_blank">Github</a>
-            <a href="https://www.instagram.com/zaphod_bobbybrox/?hl=en" target="_blank">Instagram</a>
-            <a href="https://open.spotify.com/show/2ip0Ojn7AwzukeawerfPDE" target="_blank">Spotify</a>
-            <a href="https://main.d6xxctk1tgw8o.amplifyapp.com/" target="_blank">Meyart</a> */}
+            <a href="https://github.com/Zaphod1977/" target="_blank">by...B-Squared</a>
         </div>
     </footer>
   );
